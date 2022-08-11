c2b8a2b23b7e26493f4788de622bed0e92b6c476
Christian Franco <c.franco@indexceed.com>
2022-08-11 15:35:46 -0500

    responsive text generated

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -24,31 +24,31 @@ export const TodoApp = () => {
                         <div>
                             <ul className='listcontainer'>
                                 <li className='gray'>
-                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <input className='checkbox' type="checkbox"/>
                                     <span> walk the dog </span>
                                     <button className='editButton'><i className="bi bi-pencil-square"></i></button>
                                     <button className='deleteButton'><i className="bi bi-trash"></i></button>
                                 </li>
                                 <li>
-                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <input className='checkbox' type="checkbox"/>
                                     <span> authenticate with passport-js </span>
                                     <button className='editButton'><i className="bi bi-pencil-square"></i></button>
                                     <button className='deleteButton'><i className="bi bi-trash"></i></button>
                                 </li >
                                 <li className='gray'>
-                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <input className='checkbox' type="checkbox"/>
                                     <span> validate register input </span>
                                     <button className='editButton'><i className="bi bi-pencil-square"></i></button>
                                     <button className='deleteButton'><i className="bi bi-trash"></i></button>
                                 </li>
                                 <li>
-                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <input className='checkbox' type="checkbox"/>
                                     <span> create reddit wesite </span>
                                     <button className='editButton'><i className="bi bi-pencil-square"></i></button>
                                     <button className='deleteButton'><i className="bi bi-trash"></i></button>
                                 </li>
                                 <li className='gray'>
-                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <input className='checkbox' type="checkbox"/>
                                     <span> store sessions on mongodb </span>
                                     <button className='editButton'><i className="bi bi-pencil-square"></i></button>
                                     <button className='deleteButton'><i className="bi bi-trash"></i></button>
