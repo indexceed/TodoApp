48ee79ce0fb1b01ceed57ef666f9c4d27b3d2fd2
Christian Franco <c.franco@indexceed.com>
2022-08-11 15:21:00 -0500

    list designed

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -11,23 +11,49 @@ export const TodoApp = () => {
                 <div className="mainContainer">
 
                     <div className="headContainer">
-                        <h4>Todos (5)</h4>
+                        <h3>Todos (5)</h3>
                     </div>
 
                     <div>
 
                         <div className='submitContainer'>
-                            <input type="text" placeholder='Enter todo here'/>
-                            <button>Submit</button>
+                            <input className='inputTodo' type="text" placeholder='Enter todo here'/>
+                            <button className='submitButton'>Submit</button>
                         </div>
                         
                         <div>
                             <ul className='listcontainer'>
-                                <li>walk the dog</li>
-                                <li>authenticate with passport-js</li>
-                                <li>validate register input</li>
-                                <li>create reddit wesite</li>
-                                <li>store sessions on mongodb</li>
+                                <li className='gray'>
+                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <span> walk the dog </span>
+                                    <button className='editButton'><i className="bi bi-pencil-square"></i></button>
+                                    <button className='deleteButton'><i className="bi bi-trash"></i></button>
+                                </li>
+                                <li>
+                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <span> authenticate with passport-js </span>
+                                    <button className='editButton'><i className="bi bi-pencil-square"></i></button>
+                                    <button className='deleteButton'><i className="bi bi-trash"></i></button>
+                                </li >
+                                <li className='gray'>
+                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <span> validate register input </span>
+                                    <button className='editButton'><i className="bi bi-pencil-square"></i></button>
+                                    <button className='deleteButton'><i className="bi bi-trash"></i></button>
+                                </li>
+                                <li>
+                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <span> create reddit wesite </span>
+                                    <button className='editButton'><i className="bi bi-pencil-square"></i></button>
+                                    <button className='deleteButton'><i className="bi bi-trash"></i></button>
+                                </li>
+                                <li className='gray'>
+                                    <input className='checkbox' type="checkbox" checked={true} />
+                                    <span> store sessions on mongodb </span>
+                                    <button className='editButton'><i className="bi bi-pencil-square"></i></button>
+                                    <button className='deleteButton'><i className="bi bi-trash"></i></button>
+                                </li>
+                                
                             </ul>
                         </div>
 
