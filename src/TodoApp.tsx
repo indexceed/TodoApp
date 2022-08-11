fb5a04b32258786d19c0250d111f0722e8f66bcf
Christian Franco <c.franco@indexceed.com>
2022-08-10 19:18:27 -0500

    submit designed

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -6,27 +6,36 @@ export const TodoApp = () => {
 
   return (
     <>
-        <div className="container">
-            <div className="head">
-                <h4>Todos (5)</h4>
-            </div>
-            <div>
-                <input type="text" placeholder='Enter todo here'/>
+        <div>
+            <div className='container'>
+                <div className="mainContainer">
 
-                <div>
-                    <ul className='listcontainer'>
-                    <li>walk the dog</li>
-                    <li>authenticate with passport-js</li>
-                    <li>validate register input</li>
-                    <li>create reddit wesite</li>
-                    <li>store sessions on mongodb</li>
-                </ul>
+                    <div className="headContainer">
+                        <h4>Todos (5)</h4>
+                    </div>
 
+                    <div>
+
+                        <div className='submitContainer'>
+                            <input type="text" placeholder='Enter todo here'/>
+                            <button>Submit</button>
+                        </div>
+                        
+                        <div>
+                            <ul className='listcontainer'>
+                                <li>walk the dog</li>
+                                <li>authenticate with passport-js</li>
+                                <li>validate register input</li>
+                                <li>create reddit wesite</li>
+                                <li>store sessions on mongodb</li>
+                            </ul>
+                        </div>
+
+                    </div>
                 </div>
-                
             </div>
-
         </div>
+        
     </>
     
   )
